2879c550fbec19239e5c0117182c12f6913e4b1d
Peywan Sabir <peywan1999@icloud.com>
2023-12-17 01:36:08 +0100

    changes

---
+++ modalDialog2/app.js
@@ -0,0 +1,8 @@
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
